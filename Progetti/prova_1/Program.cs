a0945d6415a2e146979ae9670bbc188fdb90a435
jean paul <paolinide@yahoo.it>
2019-02-19 22:02:50 +0100

    Initial commit

---
+++ Progetti/prova_1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace prova_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // generazione di un vettore casuale
+            int l = 5 + dado(11); // numero di elemanti tra 5 e 15
+            int[] vett1 = new int[l];
+            for (int i = 0; i < l; i++)
+                vett1[i] = dado(l);
+            StampaVettore(vett1);
+            vett1 = RiordinaVettore(vett1);
+            StampaVettore(vett1);
+
+            int[] vett2 = CopiaVettore(vett1);
+            StampaVettore(vett2);
+            ReversaVettore(ref vett2);
+            StampaVettore(vett2);
+        }
+
+
+
+        static public int dado(int max)
+        {
+            Random rnd = new Random();
+            return rnd.Next(max);
+        }
+
+
+
+        static public void StampaVettore<T>(T[] vettore, bool inLinea = true)
+        {
+            for (int i = 0; i < vettore.Length; i++)
+                Console.Write("{1,2}{2}", i, vettore[i], (inLinea) ? " " : "\n");
+            if (inLinea) Console.WriteLine();
+        }
+        static public T[] CopiaVettore<T>(T[] vettore)
+        {
+            T[] risultato = new T[vettore.Length];
+            for (int i = 0; i < vettore.Length; i++)
+                risultato[i] = vettore[i];
+            return risultato;
+        }
+        static public void ReversaVettoreVecchio<T>(ref T[] vettore)
+        {
+            T temp;
+            int l = vettore.Length;
+            for (int i = 0; i < l / 2; i++)
+            {
+                //Console.WriteLine(i);
+                temp = vettore[i];
+                vettore[i] = vettore[l - i - 1];
+                vettore[l - i - 1] = temp;
+            }
+        }
+        static public void ReversaVettore<T>(ref T[] vettore) { for (int i = 0; i < vettore.Length / 2; i++) Scambia(ref vettore[i], ref vettore[vettore.Length - i - 1]); }
+        static public void Scambia<T>(ref T primo, ref T secondo)
+        {
+            T temp = primo;
+            primo = secondo;
+            secondo = temp;
+        }
+        static public T[] RiordinaVettore<T>(T[] vettore)
+        {
+            T[] risultato = new T[vettore.Length];
+            for (int j = 0; j < vettore.Length - 1; j++)
+            {
+                Console.Write(j + ": ");
+                for (int i = 0; i < vettore.Length - 1 - j; i++)
+                {
+                    Console.Write(" {0}:{1}_{2}", i, vettore[i], vettore[i+1]);
+                }
+                Console.WriteLine();
+            }
+            return risultato;
+        }
+    }
+}
---
+++ Progetti/prova_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prova_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

38d84bb71e0c89b9d1869a45a2fa8d2fe77fd2b2
jean paul <paolinide@yahoo.it>
2019-03-03 20:13:43 +0100

    ultimo
    ultimo

--- Progetti/Tissino2/Program.cs
+++ Progetti/Tissino2/Program.cs
@@ -6,8 +6,18 @@ namespace Tissino2
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            int[] v1 = { 2, 7, 9, 25, 25, 26 }, v2 = { 8, 12, 32 }, v3 = new int[v1.Length + v2.Length];
+            int i1 = 0, i2 = 0, i3 = 0;
+            do
+            {
+                if (i1 == v1.Length) v3[i3++] = v2[i2++];
+                else if (i2 == v2.Length) v3[i3++] = v1[i1++];
+                else v3[i3++] = (v1[i1] < v2[i2]) ? v1[i1++] : v2[i2++];
+            } while (i1 < v1.Length && i2 < v2.Length);
             
-            Console.WriteLine("Hello World!");
+            for (int i=0;i<v3.Length;i++)
+                Console.WriteLine("{0} ",v3[i]);
         }
     }
 }

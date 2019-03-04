14eef6fae0855a8e6e943cfcc00018ce61eb7f7a
jean paul <paolinide@yahoo.it>
2019-03-04 09:04:59 +0100

    ultimo

--- Progetti/Tissino2/Program.cs
+++ Progetti/Tissino2/Program.cs
@@ -7,17 +7,30 @@ namespace Tissino2
         static void Main(string[] args)
         {
             Console.Clear();
-            int[] v1 = { 2, 7, 9, 25, 25, 26 }, v2 = { 8, 12, 32 }, v3 = new int[v1.Length + v2.Length];
-            int i1 = 0, i2 = 0, i3 = 0;
-            do
+            var casuale = new Random();
+            Console.WriteLine("numero casuale: " + casuale.Next(100));
+            int[] v1 = { 2, 7, 9, 13, 20, 25, 26 }, v2 = { 8, 12, 22 }, v3 = new int[v1.Length + v2.Length];
+            Console.WriteLine("max1:{0} max2:{1}", v1.Length, v2.Length);
+            for (int i1 = 0, i2 = 0; i1 + i2 < v1.Length + v2.Length;)
             {
-                if (i1 == v1.Length) v3[i3++] = v2[i2++];
-                else if (i2 == v2.Length) v3[i3++] = v1[i1++];
-                else v3[i3++] = (v1[i1] < v2[i2]) ? v1[i1++] : v2[i2++];
-            } while (i1 < v1.Length && i2 < v2.Length);
-            
-            for (int i=0;i<v3.Length;i++)
-                Console.WriteLine("{0} ",v3[i]);
+                Console.Write(" ({4}) [{0}:{1}][{2}:{3}]->", i1, ((i1 < v1.Length) ? v1[i1] : 0), i2, ((i2 < v2.Length) ? v2[i2] : 0), 1 + i1 + i2);
+                if (i1 == v1.Length) v3[i1 + i2] = v2[i2++];
+                else if (i2 == v2.Length) v3[i1 + i2] = v1[i1++];
+                else v3[i1 + i2] = ((v1[i1] < v2[i2]) ? v1[i1++] : v2[i2++]);
+
+                Console.Write("[{0}:{1}] ", i1 + i2 - 1, v3[i1 + i2 - 1]);
+
+            }
+            Console.Write("\n1° vettore: ");
+            for (int i = 0; i < v1.Length; i++)
+                Console.Write("{0} ", v1[i]);
+            Console.Write("\n2° vettore: ");
+            for (int i = 0; i < v2.Length; i++)
+                Console.Write("{0} ", v2[i]);
+            Console.Write("\n3° vettore: ");
+            for (int i = 0; i < v3.Length; i++)
+                Console.Write("{0} ", v3[i]);
+            Console.WriteLine();
         }
     }
 }

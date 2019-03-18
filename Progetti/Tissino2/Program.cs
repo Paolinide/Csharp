20cbec43dea53a46dc0a67bfd2048fdf608c0131
jean paul <paolinide@yahoo.it>
2019-03-18 14:11:27 +0100

    ultimo

--- Progetti/Tissino2/Program.cs
+++ Progetti/Tissino2/Program.cs
@@ -7,27 +7,52 @@ namespace Tissino2
         static void Main(string[] args)
         {
             Console.Clear();
-            var casuale = new Random();
+            //var casuale = new Random();
             //Console.WriteLine("numero casuale: " + casuale.Next(100));
             int[] v1 = { 2, 7, 9, 13, 20, 25, 26 }, v2 = { 8, 12, 22 }, v3 = new int[v1.Length + v2.Length];
-            //Console.WriteLine("max1:{0} max2:{1}", v1.Length, v2.Length);
+
+            v3 = Fondi(v1, v2);
+            Console.Write("1° vettore: ");
+            StampaVettore(v1);
+            Console.WriteLine("2° vettore: " + string.Join(", ", v2));
+            Console.Write("3° vettore: ");
+            StampaVettore(v3);
+            Console.WriteLine();
+
+            v1 = new int[] { 1, 1, 3, 3, 3, 2, 2, 6 };
+            v2 = new int[] { 6, 9, 23, 23, 23, 23, 46, 2 };
+            StampaSomme(v1, v2);
+        }
+        static public void StampaSomme(int[] v1, int[] v2)
+        {
+            if (v1.Length == 0 || v2.Length == 0) return;
+            int n = v1[0], somma = 0, length = Math.Min(v1.Length, v2.Length);
+            Console.Write("{0,3}: ", v1[n]);
+            for (int i = 0; i < length; i++)
+                if (v1[i] == n) somma += v2[i];
+                else
+                {
+                    n = v1[i];
+                    Console.Write("{0,5}\n{1,3}: ", somma, n);
+                    somma = v2[i];
+                }
+            Console.Write("{0,5}\n", somma);
+        }
+        static public int[] Fondi(int[] v1, int[] v2)
+        {
+            int[] v3 = new int[v1.Length + v2.Length];
             for (int i1 = 0, i2 = 0; i1 + i2 < v1.Length + v2.Length;)
             {
-                //Console.Write(" ({4}) [{0}:{1}][{2}:{3}]->", i1, ((i1 < v1.Length) ? v1[i1] : 0), i2, ((i2 < v2.Length) ? v2[i2] : 0), 1 + i1 + i2);
                 if (i1 == v1.Length) v3[i1 + i2] = v2[i2++];
                 else if (i2 == v2.Length) v3[i1 + i2] = v1[i1++];
                 else v3[i1 + i2] = ((v1[i1] < v2[i2]) ? v1[i1++] : v2[i2++]);
-                //Console.Write("[{0}:{1}] ", i1 + i2 - 1, v3[i1 + i2 - 1]);
             }
-            Console.Write("\n1° vettore: ");
-            for (int i = 0; i < v1.Length; i++)
-                Console.Write("{0} ", v1[i]);
-            Console.Write("\n2° vettore: ");
-            for (int i = 0; i < v2.Length; i++)
-                Console.Write("{0} ", v2[i]);
-            Console.Write("\n3° vettore: ");
-            for (int i = 0; i < v3.Length; i++)
-                Console.Write("{0} ", v3[i]);
+            return v3;
+        }
+        static public void StampaVettore(int[] vettore)
+        {
+            for (int i = 0; i < vettore.Length; i++)
+                Console.Write("{0} ", vettore[i]);
             Console.WriteLine();
         }
     }
---
+++ Progetti/Tissino3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Tissino3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            var casuale = new Random();
+            int[] vettore = new int[5 + casuale.Next(5)];
+            int n = 0;
+            for (int i = 0; i < vettore.Length; i++)
+            {
+                n += (int)Math.Pow(1 + casuale.Next(3), casuale.Next(3));
+                vettore[i] = n;
+            }
+
+            StampaVettore(vettore);
+            MostraDispari(vettore);
+            Console.WriteLine("Trovati " + ContaDispari(vettore) + " numeri dispari.");
+            Console.WriteLine("Il primo numero dispari, {0}, è al {1} posto.", TrovaPrimoDispari(vettore), TrovaPrimoDispari(vettore, true));
+            Console.WriteLine("L'ultimo numero dispari, {0}, è al {1} posto.", TrovaUltimoDispari(vettore), TrovaUltimoDispari(vettore, true));
+            Console.WriteLine("Il numero dispari più grosso, {0}, è al {1} posto.", TrovaDispariPiuGrosso(vettore), TrovaDispariPiuGrosso(vettore, true));
+        }
+        static public void StampaVettore(int[] vettore)
+        {
+            for (int i = 0; i < vettore.Length; i++)
+                Console.Write(vettore[i] + " ");
+            Console.WriteLine();
+        }
+        static public void MostraDispari(int[] vettore)
+        {
+            Console.WriteLine("Dispari!");
+            return;
+            for (int i = 0; i < vettore.Length; i++)
+                if (vettore[i] % 2 == 1) Console.Write(vettore[i] + " ");
+            Console.WriteLine();
+        }
+        static public int ContaDispari(int[] vettore)
+        {
+            int risultato = 0;
+            for (int i = 0; i < vettore.Length; i++)
+                if (vettore[i] % 2 == 1) risultato++;
+            return risultato;
+        }
+        static public bool TrovaPrimoDispari(int[] vettore, ref int risultato, bool posizione = false)
+        {
+            for (int i = 0; i < vettore.Length; i++)
+                if (vettore[i] % 2 == 1) { risultato = (posizione ? i : vettore[i]); return true; }
+            return false;
+        }
+        static public bool TrovaUltimoDispari(int[] vettore, ref int risultato, bool posizione = false)
+        {
+            bool trovato = false;
+            for (int i = 0; i < vettore.Length; i++)
+                if (vettore[i] % 2 == 1) { risultato = posizione ? i : vettore[i]; trovato = true}
+            return trovato;
+        }
+        static public int TrovaDispariPiuGrosso(int[] vettore, bool posizione = false)
+        {
+            int risultato = -1;
+            return risultato;
+        }
+    }
+}
